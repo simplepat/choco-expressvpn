0c8b86958483793b5734b63832af1f3c167c80a1
Patrick Marshall <patrick.marshall@hotmail.fr>
2019-01-20 22:58:16 +0800

    Initial Commit

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://download.expressvpn.xyz/clients/windows/expressvpn_6.8.5.5936.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url           = $url
+  softwareName  = 'ExpressVPN*'
+  checksum      = '77b1f7e25bd961cbcad410aaf25ff9d4'
+  checksumType  = 'md5'
+  silentArgs   = '/S'
+}
+
+Install-ChocolateyPackage @packageArgs
