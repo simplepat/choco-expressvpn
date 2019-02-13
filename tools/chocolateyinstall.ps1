10f5703c559c98e08bca2264e4f370b199dda54b
Patrick Marshall <patrick.marshall@hotmail.fr>
2019-02-14 07:41:51 +1000

    Updated package to newest version

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://download.expressvpn.xyz/clients/windows/expressvpn_6.8.5.5936.exe'
+$url        = 'https://download.expressvpn.xyz/clients/windows/expressvpn_6.8.6.6583.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -8,7 +8,7 @@ $packageArgs = @{
   fileType      = 'exe'
   url           = $url
   softwareName  = 'ExpressVPN*'
-  checksum      = '77b1f7e25bd961cbcad410aaf25ff9d4'
+  checksum      = '72bfa1bb1395425b1f45a5be8b2850d1'
   checksumType  = 'md5'
   silentArgs   = '/S'
 }
